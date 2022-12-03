e4d504103714c2ebdb4038cc4dd7430bfa120969
shuruk421 <32776970+shuruk421@users.noreply.github.com>
2022-12-03 18:44:28 +0200

    initial commit

---
+++ Euclidean.h
@@ -0,0 +1,20 @@
+//
+// Created by Ahmad Jorban on 10/11/2022.
+//
+
+#ifndef UNTITLED8_EUCLIDEAN_H
+#define UNTITLED8_EUCLIDEAN_H
+
+#include <vector>
+#include "DistanceFunction.h"
+
+using namespace std;
+
+//Euclidean distance function class
+class Euclidean : public DistanceFunction {
+public:
+    virtual double Distance(vector<float> vector1, vector<float> vector2);
+};
+
+
+#endif //UNTITLED8_EUCLIDEAN_H
---
+++ Manhatin.h
@@ -0,0 +1,20 @@
+//
+// Created by Ahmad Jorban on 10/11/2022.
+//
+
+#ifndef UNTITLED8_MANHATIN_H
+#define UNTITLED8_MANHATIN_H
+
+#include "DistanceFunction.h"
+#include <vector>
+
+using namespace std;
+
+//Manhatin distance function class
+class Manhatin : public DistanceFunction {
+public:
+    virtual double Distance(vector<float> vector1, vector<float> vector2);
+};
+
+
+#endif //UNTITLED8_MANHATIN_H
